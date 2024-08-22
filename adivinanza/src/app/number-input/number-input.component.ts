1d1318cf691a1e854d129f7c6be70410432a94d9
Nicolas Nievas <nicoonievas7@gmail.com>
2024-08-22 15:16:20 -0300

    emitir datos component number input

--- adivinanza/src/app/number-input/number-input.component.ts
+++ adivinanza/src/app/number-input/number-input.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-number-input',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './number-input.component.html',
   styleUrl: './number-input.component.css'
 })
 export class NumberInputComponent {
+  @Output() guessMade = new EventEmitter<number>();
+  guess : number | undefined;
 
+  makeGuess() {
+    this.guessMade.emit(this.guess);
+    this.guess = undefined;
+  }
 }
